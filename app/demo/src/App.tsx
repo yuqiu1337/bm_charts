f38004ddefd23f9ce81c4697dc6f028ace66bdd8
levi <levidcd@outlook.com>
2022-07-18 14:46:20 +0800

    chore: demo

---
+++ app/demo/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BmCharts from '@bm/core';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+      </div>
+    </div>
+  );
+}
+
+export default App;
