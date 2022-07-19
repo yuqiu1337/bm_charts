531a1547f6a7fe14559dbf4691d5921dd0f491fb
levi <levidcd@outlook.com>
2022-07-19 14:29:55 +0800

    chore: vue构建（暂）

---
+++ packages/vue/src/BaseChart/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+<script>
+export default {
+  name: "BaseChart",
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  components: {},
+  props: {},
+  beforeMount: function () {},
+  created: function () {},
+  mounted: function () {},
+  update: function () {},
+  destroy: function () {},
+};
+</script>
+<style lang="less" scoped></style>
---
+++ packages/vue/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @date          Invalid Date
+ * Copyright © YourCompanyName All rights reserved
+ */
+import BaseChart from "./BaseChart/index.vue";
+export { BaseChart };
