b1643e63c5a239ce8963eae2b1bafa779fababbe
levi <levidcd@outlook.com>
2022-07-21 11:14:39 +0800

    feat: 调整项目

--- packages/react-lib/.fatherrc.ts
+++ packages/react-lib/.fatherrc.ts
@@ -1,5 +1,9 @@
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  esm: {},
-  umd: {}
+  esm: {
+    output: 'es',
+  },
+  umd: {
+    output: 'umd',
+  },
 };
