7fdaeff7d48cae948d011381a78e53fea958d190
levi <levidcd@outlook.com>
2022-07-22 10:59:24 +0800

    feat: 添加测试用例

--- packages/react/src/components/BarChart/index.test.tsx
+++ packages/react/src/components/BarChart/index.test.tsx
@@ -1,20 +1,31 @@
 /**
- * @date          Invalid Date
+ * @author        levi <levidcd@outlook.com>
+ * @date          2022-07-21 19:19:16
  * Copyright © YourCompanyName All rights reserved
  */
 import React from 'react';
-import { render } from '@testing-library/react';
+import {
+  getByRole,
+  getByTestId,
+  findByRole,
+  render,
+  screen,
+  findByTestId,
+} from '@testing-library/react';
 import BarChart from './index';
 
-test('11', () => {
+test('barChart渲染到页面上', async () => {
+  const containerClass = 'test-component';
   render(
     <BarChart
       title={''}
+      data-testid={'11221'}
       id={''}
       initOptions={{}}
       initOnLoad={function (): void {
         throw new Error('Function not implemented.');
       }}
+      containerClass={containerClass}
       params={{}}
       headers={{}}
       action={''}
@@ -23,4 +34,10 @@ test('11', () => {
       }}
     />,
   );
+  const el = await screen.findAllByTestId('11221');
+  console.log(el);
+  // el.then((ele) => {
+  //   console.log(1);
+  expect(el).toBeInTheDocument();
+  // });
 });
